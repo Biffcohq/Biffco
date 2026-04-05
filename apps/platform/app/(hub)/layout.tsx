f3d2c089c331c4b0c24bf5aa110d2421793b11bc
BiffcoDev <biffcodev@gmail.com>
2026-04-05 12:12:12 -0300

    feat(hub): restore sidebar navigation to global hub

--- apps/platform/app/(hub)/layout.tsx
+++ apps/platform/app/(hub)/layout.tsx
@@ -1,11 +1,12 @@
 import { Topbar } from '../components/layout/Topbar'
+import { Sidebar } from '../components/layout/Sidebar'
 import { AuthGuard } from '../components/auth/AuthGuard'
 
 export default function HubLayout({ children }: { children: React.ReactNode }) {
   return (
     <AuthGuard>
       <div className="flex h-screen bg-bg-base overflow-hidden">
-        {/* We don't need a Vertical Sidebar for the Global Hub */}
+        <Sidebar />
         <div className="flex-1 flex flex-col min-w-0">
           <Topbar />
           <main className="flex-1 overflow-x-hidden overflow-y-auto">
